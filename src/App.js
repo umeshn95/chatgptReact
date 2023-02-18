458bcfc2a3d07cc4d671086793a157c44dae2922
umeshn95 <umeshnamdev9@gmail.com>
2023-02-18 21:38:31 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [prompt, setPrompt] = useState("");
+  const [text, setText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const apiKey = "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";
+  const model = "text-davinci-002";
+
+  const handlePromptChange = (event) => {
+    setPrompt(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setIsLoading(true);
+    fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        model: model,
+        max_tokens: 1000,
+        temperature: 0.5
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      const text = data.choices[0].text;
+      setText(text);
+    })
+    .catch(error => console.error(error))
+    .finally(() => setIsLoading(false));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>
+        Enter a prompt:
+        <input type="text" value={prompt} onChange={handlePromptChange} />
+      </label>
+      <button onClick={handleButtonClick} disabled={isLoading}>
+        Generate text
+      </button>
+      {isLoading && <p>Loading...</p>}
+      {text && <p>{text}</p>}
     </div>
   );
-}
+};
 
 export default App;
